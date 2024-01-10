799dbe1a925b1733c5e68030cb680107ba64dedd
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2024-01-10 15:48:16 +0530

    Add files via upload

---
+++ version_check.py
@@ -0,0 +1,15 @@
+ # To check the version of python 
+
+import sys 
+print('python:{}'.format(sys.version))
+import scipy
+print('scipy:{}'.format(scipy._version_))
+import numpy
+print('numpy:{}'.format(numpy._version_))
+import matplotlib
+print('matplotlib:{}'.format(matplotlib._version_))
+import pandas
+print('pandas:{}'.format(pandas._version_))
+import sklearn
+print('sklearn:{}'.format(sklearn._version_))
+
