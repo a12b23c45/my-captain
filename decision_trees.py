88846bd7ff659478b7e898a08427d80f2eb1798f
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2024-01-11 22:36:51 +0530

    Add files via upload

--- decision_trees.py
+++ decision_trees.py
@@ -6,7 +6,7 @@ import scipy
 print('scipy:{}'.format(scipy._version_))
 import numpy
 print('numpy:{}'.format(numpy._version_))
-import matplotlib
+import matplotlib 
 print('matplotlib:{}'.format(matplotlib._version_))
 import pandas
 print('pandas:{}'.format(pandas._version_))
@@ -46,3 +46,65 @@ print(dataset.head(20))
 
 #statistical summary
 print(dataset.describe())
+
+#class distribution(no. of rows )
+print(dataset.groupby('class').size())
+
+#univariate plots -box and whisker plots
+dataset.plot(kind='box',subplots=True,layout=(2,2),sharex=False,sharey=False)
+pyplot.show()
+
+#histograph of the variable
+dataset.hist()
+pyplot.show()
+
+#multivariate plot
+scatter_matrix(dataset)
+pyplot.show()
+
+#creating a validation dataset
+#splitting dataset
+array = dataset.values
+X = array[:, 0:4]
+y = array[:, 4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.2, random_state=1)
+
+# Logistic Regression
+# Linear Discriminant Analysis
+# K-Nearest neighbors
+# Classification and Regression Trees
+# Gaussian Naive Bayes
+# Support Vector Machines
+
+# building models
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+
+# evaluate the created models
+results = []
+names = []
+for name, model in models:
+   kfold = StratifiedKFold(n_splits=10, random_state=1) 
+   cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+   results.append(cv_results)
+   names.append(name)
+   print('%s: %f (%f)' % (name, cvresults.mean(), cv_results.std()))
+
+#compare our models
+pyplot.boxplot(results,labels=names)
+pyplot.title('Algorithm comparison')
+pyplot.show()
+
+#make prediction on svm
+model=SVC(gamma='auto')
+model.fix(X_train,Y_train)
+predictions = model.predict(X_validation)
+
+#evaluate prediction
+print(accuracy_score(Y_validation,predictions))
+print(confusion_matrix(Y_validation,predictions))
+print(classification_report(Y_validation,predictions))
